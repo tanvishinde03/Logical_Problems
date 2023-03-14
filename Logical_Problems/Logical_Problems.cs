c2d41650b452db58c2168346bd36f68b6cb3e6c3
tanvishinde03 <shindetanvi0203@gmail.com>
2023-03-14 18:59:32 +0530

    [Tanvi] Add : UC8 - Checking Sum of Digit

--- Logical_Problems/Logical_Problems.cs
+++ Logical_Problems/Logical_Problems.cs
@@ -125,8 +125,22 @@ namespace Logical_Problems
             }
             Console.ReadLine();
         }
+        public void CheckSumDigit()
+        {
+            int n, sum = 0, m;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+            Console.Write("Sum is= " + sum);
+        }
     }
 }
+
     
     
 
