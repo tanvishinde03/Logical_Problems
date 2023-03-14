6f749a5192df12e87d586463903a43fcae827a6c
tanvishinde03 <shindetanvi0203@gmail.com>
2023-03-14 17:46:18 +0530

    [Tanvi] Add : UC1 - Checking whether number is Even or Odd

---
+++ Logical_Problems/Logical_Problems.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Problems
+{
+    public class Logical_Problems
+    {
+        public void CheckEvenOrOdd()
+        {
+            int Number;
+            Console.Write("Enter The Number: ");
+            Number = int.Parse(Console.ReadLine());
+            if (Number % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+            }
+            Console.Read();
+        }
+    }
+
+}
+
---
+++ Logical_Problems/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Problems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Logical_Problems logicalproblems = new Logical_Problems();
+            logicalproblems.CheckEvenOrOdd();
+            Console.ReadLine();
+                    
+
+        }
+        }
+    }
+
